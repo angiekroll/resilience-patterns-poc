3faf646fecb0226f65f21a341f35613c86cf7748
AngelaCastillo <angela.castillo@appgate.com>
2025-10-03 10:58:59 -0500

    ✨ feat(poc): init project configuration

---
+++ src/main/java/com/resiliencepatterns/poc/ResiliencePatternsPocApplication.java
@@ -0,0 +1,13 @@
+package com.resiliencepatterns.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResiliencePatternsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResiliencePatternsPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resiliencepatterns/poc/ResiliencePatternsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resiliencepatterns.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResiliencePatternsPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
